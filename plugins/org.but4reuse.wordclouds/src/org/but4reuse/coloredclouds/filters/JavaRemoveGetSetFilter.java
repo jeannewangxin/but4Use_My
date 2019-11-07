505a8921add1de392f0c087979ccb071ee3df34e
NicolasMatton <nicomatton55@gmail.com>
2019-11-07 13:13:34 +0100

    update colored cloud

---
+++ plugins/org.but4reuse.wordclouds/src/org/but4reuse/coloredclouds/filters/JavaRemoveGetSetFilter.java
@@ -0,0 +1,23 @@
+package org.but4reuse.coloredclouds.filters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class JavaRemoveGetSetFilter implements IWordsProcessing {
+
+	@Override
+	public List<String> processWords(List<String> words, IProgressMonitor monitor) {
+		List<String> result=new ArrayList<String>();
+		for(String word : words) {
+			if(word.length()>=3 && (word.substring(0, 3).equals("get") || word.substring(0, 3).equals("set"))) {
+				word = word.substring(3);
+			}
+			result.add(word);
+		}
+		return result;
+	}
+
+
+}
---
+++ plugins/org.but4reuse.wordclouds/src/org/but4reuse/coloredclouds/filters/WordCloudFiltersHelper.java
@@ -0,0 +1,108 @@
+package org.but4reuse.coloredclouds.filters;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.but4reuse.coloredclouds.activator.Activator;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+/**
+ * Word cloud filters Helper
+ * 
+ * @author jabier.martinez
+ * 
+ */
+public class WordCloudFiltersHelper {
+
+	public static final String FILTER_EXTENSIONPOINT = "org.but4reuse.wordclouds.filter";
+
+	static List<IWordsProcessing> cache_wordsprocessing;
+
+	/**
+	 * Get all the registered filters
+	 * 
+	 * @return
+	 */
+	public static List<IWordsProcessing> getAllFilters() {
+		if (cache_wordsprocessing != null) {
+			return cache_wordsprocessing;
+		}
+		List<IWordsProcessing> visualisations = new ArrayList<IWordsProcessing>();
+		IConfigurationElement[] visualisationExtensionPoints = Platform.getExtensionRegistry()
+				.getConfigurationElementsFor(FILTER_EXTENSIONPOINT);
+		for (IConfigurationElement visualisationExtensionPoint : visualisationExtensionPoints) {
+			try {
+				visualisations.add((IWordsProcessing) visualisationExtensionPoint.createExecutableExtension("class"));
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+		}
+		cache_wordsprocessing = visualisations;
+		return visualisations;
+	}
+
+	public static IPreferenceStore getPreferenceStore() {
+		return Activator.getDefault().getPreferenceStore();
+	}
+
+	public static String getFilterName(IWordsProcessing algo) {
+		IConfigurationElement[] adapterExtensionPoints = Platform.getExtensionRegistry()
+				.getConfigurationElementsFor(FILTER_EXTENSIONPOINT);
+		for (IConfigurationElement adapterExtensionPoint : adapterExtensionPoints) {
+			try {
+				IWordsProcessing oneAlgo = (IWordsProcessing) adapterExtensionPoint.createExecutableExtension("class");
+				if (oneAlgo.getClass().equals(algo.getClass())) {
+					String name = adapterExtensionPoint.getAttribute("name");
+					return name;
+				}
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+		}
+		return "No name";
+	}
+
+	public static List<IWordsProcessing> getSortedSelectedFilters() {
+		Map<IWordsProcessing, Integer> map = new LinkedHashMap<IWordsProcessing, Integer>();
+		for (IWordsProcessing algo : getAllFilters()) {
+			System.out.println("algo name : "+algo.getClass().getName());
+			String name = getFilterName(algo);
+			System.out.println("filter name : "+name);
+			IPreferenceStore prefs = getPreferenceStore();
+			int priority = prefs.getInt(name);
+			System.out.println("filter priority : "+priority);
+			// 0 means not selected
+			if (priority > 0) {
+				map.put(algo, priority);
+			}
+		}
+
+		// sort a hashmap by the values
+		List<Map.Entry<IWordsProcessing, Integer>> entries = new ArrayList<Map.Entry<IWordsProcessing, Integer>>(
+				map.entrySet());
+		Collections.sort(entries, new Comparator<Map.Entry<IWordsProcessing, Integer>>() {
+			public int compare(Map.Entry<IWordsProcessing, Integer> a, Map.Entry<IWordsProcessing, Integer> b) {
+				return b.getValue().compareTo(a.getValue());
+			}
+		});
+		LinkedHashMap<IWordsProcessing, Integer> sortedMap = new LinkedHashMap<IWordsProcessing, Integer>();
+		for (Map.Entry<IWordsProcessing, Integer> entry : entries) {
+			sortedMap.put(entry.getKey(), entry.getValue());
+		}
+
+		// from set to list
+		List<IWordsProcessing> toReturn = new ArrayList<IWordsProcessing>();
+		for (IWordsProcessing wp : sortedMap.keySet()) {
+			toReturn.add(wp);
+		}
+		return toReturn;
+	}
+
+}
